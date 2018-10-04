b1dbc1aafc439914ad8be4b1e294a004a28b2246
Siddharth Behra <siddharthrkh@gmail.com>
2018-10-05 01:18:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
 scores[activePlayer] += roundScore;
 document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
 
-if(scores[activePlayer]>=20)
+if(scores[activePlayer]>=50)
 {	
 document.querySelector('#name-'+activePlayer).textContent = "Winner!";
 document.querySelector('.dice').style.display = 'none';
